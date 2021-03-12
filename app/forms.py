2b81cf4a3af8f1832e497ccc99fc9873b90c093f
harry <harrisongitau3@gmail.com>
2021-03-12 16:42:13 +0300

    Manually create form rather than use wtf.quick_form(form)

--- app/forms.py
+++ app/forms.py
@@ -4,5 +4,6 @@ from wtforms.validators import DataRequired
 
 
 class JoinForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    submit = SubmitField('Join Call')
+    username = StringField('Username', validators=[DataRequired()],
+                           render_kw={"placeholder": "Username"})
+    submit = SubmitField('Join Call', render_kw={"placeholder": "Username"})
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
 from app import app
 from flask import render_template
-from app.forms import JoinForm
+# from app.forms import JoinForm
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/join', methods=['GET', 'POST'])
 def index():
-    form = JoinForm()
-    return render_template('index.html', title='Join Video', form=form)
+    # form = JoinForm()
+    # return render_template('index.html', title='Join Video', form=form)
+    return render_template('index.html', title='Join Video')
 
 
 @app.route('/home', methods=['POST'])
