955731759d5a87ec9102256bbcc0e19198209bd9
harry <harrisongitau3@gmail.com>
2021-03-12 18:20:34 +0300

    Load .env file

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config(object):
     START_NGROK = os.environ.get('START_NGROK') is not None
     SECRET_KEY = os.environ.get("SECRET_KEY") or 'you-will-never-guess'
+    TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+    TWILIO_API_KEY_SID = os.environ.get("TWILIO_API_KEY_SID")
+    TWILIO_API_KEY_SECRET = os.environ.get("TWILIO_API_KEY_SECRET")
