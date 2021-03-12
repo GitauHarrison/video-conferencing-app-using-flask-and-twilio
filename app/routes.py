7e19f054895505e7b7bd23e3ae5d1bcecead545c
harry <harrisongitau3@gmail.com>
2021-03-12 18:28:50 +0300

    Generate access token for participant

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, abort
 from app.forms import JoinForm
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -13,3 +15,18 @@ def index():
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home')
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    username = request.get_json(force=True).get('username')
+    if not username:
+        abort(401)
+    token = AccessToken(
+        app.config["TWILIO_ACCOUNT_SID"],
+        app.config["TWILIO_API_KEY_SID"],
+        app.config["TWILIO_API_KEY_SECRET"],
+        identity=username
+    )
+    token.add_grant(VideoGrant(room="My Room"))
+    return {'token': token.to_jwt().decode()}
