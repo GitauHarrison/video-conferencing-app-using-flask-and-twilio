413fde534a5cea0861b6dacd0a686c37c7129ed7
harry <harrisongitau3@gmail.com>
2021-03-13 05:46:51 +0300

    Listen to join button event

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,3 +1,11 @@
+let connected = false;
+const usernameInput = document.getElementById('username');
+const button = document.getElementById('join');
+const leave_call = document.getElementById('leave');
+const container = document.getElementById('video_container');
+const count = document.getElementById('count');
+let room;
+
 function addLocalVideo(){
     Twilio.Video.createLocalVideoTrack().then(track => {
         let video = document.getElementById('local').firstChild;
@@ -5,4 +13,34 @@ function addLocalVideo(){
     });
 };
 
+function connectButtonHandler(event){
+    event.preventDefault();
+    if (!connected) {
+        let username = usernameInput.value;
+        if (!username){
+            alert('Enter your username before connecting');
+            return;
+        }
+        button.disabled = true;
+        button.innerHTML = 'Connecting...';
+        window.location='/home';
+        connect(username).then(() => {
+            leave_call.innerHTML = 'Leave Call';
+            button.disabled = false;
+        }).catch(() => {
+            alert('Connection failed. Is the backend working?');
+            button.innerHTML = 'Join Call';
+            button.disabled = false;
+            window.location='/join';
+        });
+    }
+    else{
+        disconnect();
+        button.innerHTML = 'Join Call';
+        window.location='/join';
+        connected = false;
+    }
+};
+
 addLocalVideo();
+button.addEventListener('click', connectButtonHandler);
