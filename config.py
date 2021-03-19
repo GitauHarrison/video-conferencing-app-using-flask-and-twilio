7ec04a40bdbee4b596a527ebd1d4b4136ad3f327
harry <harrisongitau3@gmail.com>
2021-03-19 06:53:52 +0300

    Update mail port env variable

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
 
     # Email configurations
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
