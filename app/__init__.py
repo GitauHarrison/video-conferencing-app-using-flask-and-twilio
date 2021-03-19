45802626a9713f523d3cf04e7c535841675af57b
harry <harrisongitau3@gmail.com>
2021-03-19 06:26:12 +0300

    Improve logging errors

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from config import Config
 from flask_bootstrap import Bootstrap
 import os
 import logging
-from logging.handlers import RotatingFileHandler
+from logging.handlers import RotatingFileHandler, SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,7 +20,25 @@ def start_ngrok():
 if app.config["START_NGROK"]:
     start_ngrok()
 
-if not app.debug:
+if not app.debug and not app.testing:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'],
+                    app.config['MAIL_PASSWORD']
+                    )
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='noreply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            subject='Ona Ana Failure',
+            credentials=auth, secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
     if app.config['LOG_TO_STDOUT']:
         stream_handler = logging.StreamHandler()
         stream_handler.setLevel(logging.INFO)
