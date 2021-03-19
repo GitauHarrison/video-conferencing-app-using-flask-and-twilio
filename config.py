62d87d340092cd9bc920424b07a09c78c3dd6b6d
harry <harrisongitau3@gmail.com>
2021-03-19 06:44:04 +0300

    Add email configurations

--- config.py
+++ config.py
@@ -12,3 +12,11 @@ class Config(object):
     TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
     TWILIO_API_KEY_SID = os.environ.get("TWILIO_API_KEY_SID")
     TWILIO_API_KEY_SECRET = os.environ.get("TWILIO_API_KEY_SECRET")
+
+    # Email configurations
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
